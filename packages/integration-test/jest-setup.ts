516c70a359b3173b7e9c52e94dd52f1ea0d7c667
jemanuelp <jemanuelpachecoo@gmail.com>
2025-11-14 22:06:06 -0300

    fix(integration-test): add global TextEncoder and TextDecoder for compatibility in jest-setup

--- packages/integration-test/jest-setup.ts
+++ packages/integration-test/jest-setup.ts
@@ -1,1 +1,10 @@
 import 'reflect-metadata';
+import { TextEncoder, TextDecoder } from 'util';
+
+if (!(global as any).TextEncoder) {
+    (global as any).TextEncoder = TextEncoder;
+}
+
+if (!(global as any).TextDecoder) {
+    (global as any).TextDecoder = TextDecoder as any;
+}
